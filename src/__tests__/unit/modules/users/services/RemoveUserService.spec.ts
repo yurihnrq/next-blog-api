0c01d0c07c14a6301a8dbf31f81be9a7f8120bfd
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-06-08 16:57:59 -0300

    test: RemoveUserService should throw an error if user with provided id does not exist

--- src/__tests__/unit/modules/users/services/RemoveUserService.spec.ts
+++ src/__tests__/unit/modules/users/services/RemoveUserService.spec.ts
@@ -28,4 +28,18 @@ describe('GetUserByIdService', () => {
     expect(usersRepository.getById).toHaveBeenCalledWith(usersMock[0].id);
     expect(usersRepository.remove).toHaveBeenCalledWith(usersMock[0].id);
   });
+
+  it('should throw an error if user with provided id does not exist', async () => {
+    jest.spyOn(usersRepository, 'getById').mockImplementation(() => {
+      return Promise.resolve(null);
+    });
+
+    try {
+      await removeUserService.execute(usersMock[0].id as string);
+    } catch (error) {
+      expect((error as Error).message).toBe(
+        'User with provided id does not exist.'
+      );
+    }
+  });
 });

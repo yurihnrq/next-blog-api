26aa67f1c9dc1e0198a3858034cbf94fa1f7ebfd
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-06-08 16:45:48 -0300

    test: GetUserByIdService should throw an error if no user is found

--- src/__tests__/unit/modules/users/services/GetUserByIdService.spec.ts
+++ src/__tests__/unit/modules/users/services/GetUserByIdService.spec.ts
@@ -29,4 +29,18 @@ describe('GetUserByIdService', () => {
     expect(returnedUser).toEqual(usersMock[0]);
     expect(usersRepository.getById).toHaveBeenCalledWith(usersMock[0].id);
   });
+
+  it('should throw an error if user is not found', async () => {
+    jest.spyOn(usersRepository, 'getById').mockImplementation(() => {
+      return Promise.resolve(null);
+    });
+
+    try {
+      await getUserByIdService.execute(usersMock[0].id as string);
+    } catch (error) {
+      expect((error as Error).message).toBe(
+        'User with provided id does not exist.'
+      );
+    }
+  });
 });

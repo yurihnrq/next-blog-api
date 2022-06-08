755bad2ed7e95354b2062cc682221e7109bfc120
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-06-08 16:33:44 -0300

    test: GetAllUsersService should be able to get all users

---
+++ src/__tests__/unit/modules/users/services/GetAllUsersService.spec.ts
@@ -0,0 +1,25 @@
+import prisma from '../../../../../configs/prisma';
+import { usersMock } from '../../../../../mocks/usersMocks';
+import { IUsersRepository } from '../../../../../modules/users/repositories/interfaces/IUsersRepository';
+import { PrismaUsersRepository } from '../../../../../modules/users/repositories/PrismaUsersRepository';
+import { GetAllUsersService } from '../../../../../modules/users/services/GetAllUsersService';
+import { BCryptHashProvider } from '../../../../../providers/BCryptHashProvider';
+import { IHashProvider } from '../../../../../providers/interfaces/IHashProvider';
+
+describe('GetAllUsersService', () => {
+  const hashProvider: IHashProvider = new BCryptHashProvider();
+  const usersRepository: IUsersRepository = new PrismaUsersRepository(
+    prisma,
+    hashProvider
+  );
+  const getAllUsersService = new GetAllUsersService(usersRepository);
+
+  it('should be able to get all users', async () => {
+    jest.spyOn(usersRepository, 'getAll').mockResolvedValue(usersMock);
+
+    const users = await getAllUsersService.execute(1);
+
+    expect(users).toEqual(usersMock);
+    expect(usersRepository.getAll).toHaveBeenCalledWith(1);
+  });
+});

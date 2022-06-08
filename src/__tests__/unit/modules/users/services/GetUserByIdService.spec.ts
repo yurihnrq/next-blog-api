1a6cf76dab6e0f1299c916eae14f0e1943844ca7
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-06-08 16:42:41 -0300

    test: GetUserByIdService should return a user

---
+++ src/__tests__/unit/modules/users/services/GetUserByIdService.spec.ts
@@ -0,0 +1,32 @@
+import prisma from '../../../../../configs/prisma';
+import { usersMock } from '../../../../../mocks/usersMocks';
+import { IUsersRepository } from '../../../../../modules/users/repositories/interfaces/IUsersRepository';
+import { PrismaUsersRepository } from '../../../../../modules/users/repositories/PrismaUsersRepository';
+import { GetUserByIdService } from '../../../../../modules/users/services/GetUserByIdService';
+import { IGetUserByIdService } from '../../../../../modules/users/services/interfaces/IGetUserByIdService';
+import { BCryptHashProvider } from '../../../../../providers/BCryptHashProvider';
+import { IHashProvider } from '../../../../../providers/interfaces/IHashProvider';
+
+describe('GetUserByIdService', () => {
+  const hashProvider: IHashProvider = new BCryptHashProvider();
+  const usersRepository: IUsersRepository = new PrismaUsersRepository(
+    prisma,
+    hashProvider
+  );
+  const getUserByIdService: IGetUserByIdService = new GetUserByIdService(
+    usersRepository
+  );
+
+  it('should return a user', async () => {
+    jest.spyOn(usersRepository, 'getById').mockImplementation(() => {
+      return Promise.resolve(usersMock[0]);
+    });
+
+    const returnedUser = await getUserByIdService.execute(
+      usersMock[0].id as string
+    );
+
+    expect(returnedUser).toEqual(usersMock[0]);
+    expect(usersRepository.getById).toHaveBeenCalledWith(usersMock[0].id);
+  });
+});

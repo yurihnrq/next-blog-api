68713da375babccd05bc74949e39d86c7bec2580
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-06-08 16:55:01 -0300

    test: RemoveUserService should remove a user

---
+++ src/__tests__/unit/modules/users/services/RemoveUserService.spec.ts
@@ -0,0 +1,31 @@
+import prisma from '../../../../../configs/prisma';
+import { usersMock } from '../../../../../mocks/usersMocks';
+import { IUsersRepository } from '../../../../../modules/users/repositories/interfaces/IUsersRepository';
+import { PrismaUsersRepository } from '../../../../../modules/users/repositories/PrismaUsersRepository';
+import { IRemoveUserService } from '../../../../../modules/users/services/interfaces/IRemoveUserService';
+import { RemoveUserService } from '../../../../../modules/users/services/RemoveUserService';
+import { BCryptHashProvider } from '../../../../../providers/BCryptHashProvider';
+import { IHashProvider } from '../../../../../providers/interfaces/IHashProvider';
+
+describe('GetUserByIdService', () => {
+  const hashProvider: IHashProvider = new BCryptHashProvider();
+  const usersRepository: IUsersRepository = new PrismaUsersRepository(
+    prisma,
+    hashProvider
+  );
+  const removeUserService: IRemoveUserService = new RemoveUserService(
+    usersRepository
+  );
+
+  it('should remove a user', async () => {
+    jest.spyOn(usersRepository, 'getById').mockImplementation(() => {
+      return Promise.resolve(usersMock[0]);
+    });
+    jest.spyOn(usersRepository, 'remove').mockImplementation();
+
+    await removeUserService.execute(usersMock[0].id as string);
+
+    expect(usersRepository.getById).toHaveBeenCalledWith(usersMock[0].id);
+    expect(usersRepository.remove).toHaveBeenCalledWith(usersMock[0].id);
+  });
+});

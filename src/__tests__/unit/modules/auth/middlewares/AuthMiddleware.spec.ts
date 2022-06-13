b394c0b46e65a3c5e38c523f69e55e3350b2ef5a
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-06-13 11:07:54 -0300

    feat: AuthMiddleware should throw an APIError if no token is provided

--- src/__tests__/unit/modules/auth/middlewares/AuthMiddleware.spec.ts
+++ src/__tests__/unit/modules/auth/middlewares/AuthMiddleware.spec.ts
@@ -2,8 +2,11 @@ import { AuthMiddleware } from '@src/modules/auth/middlewares/AuthMiddleware';
 import { IMiddleware } from '@src/types/IMiddleware';
 import { requestMock } from '@mocks/express/requestMock';
 import { responseMock } from '@mocks/express/responseMock';
+import APIError from '@src/errors/APIError';
 
 const authMiddleware: IMiddleware = new AuthMiddleware();
+
+requestMock.headers.authorization = 'Bearer 123';
 
 describe('AuthMiddleware', () => {
   it('should call next function', async () => {
@@ -12,5 +15,20 @@ describe('AuthMiddleware', () => {
     await authMiddleware.execute(requestMock, responseMock, next);
 
     expect(next).toHaveBeenCalled();
+  });
+
+  it('should throw an APIError if no token is provided', async () => {
+    const next = jest.fn();
+    requestMock.headers.authorization = undefined;
+
+    try {
+      await authMiddleware.execute(requestMock, responseMock, next);
+    } catch (error) {
+      expect(error).toBeInstanceOf(APIError);
+      expect((error as APIError).status).toBe(401);
+      expect((error as APIError).message).toBe('Unauthorized request.');
+    }
+
+    expect.assertions(3);
   });
 });
--- src/modules/auth/middlewares/AuthMiddleware.ts
+++ src/modules/auth/middlewares/AuthMiddleware.ts
@@ -1,8 +1,13 @@
+import APIError from '@src/errors/APIError';
 import { IMiddleware } from '@src/types/IMiddleware';
 import { NextFunction, Request, Response } from 'express';
 
 export class AuthMiddleware implements IMiddleware {
-  execute = async (_req: Request, _res: Response, next: NextFunction) => {
+  execute = async (req: Request, _res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+
+    if (!token) throw new APIError(401, 'Unauthorized request.');
+
     next();
   };
 }

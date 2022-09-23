45a679d5fd46b9c557a15a26cba4fba28c6e5ec7
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-09-23 17:52:20 -0300

    fix: removed unnecessary code

--- src/__tests__/unit/middlewares/ExceptionMiddleware.spec.ts
+++ src/__tests__/unit/middlewares/ExceptionMiddleware.spec.ts
@@ -3,17 +3,12 @@ import { ExceptionMiddleware } from '@src/middlewares/ExceptionMiddleware';
 import { requestMock } from '@src/__mocks__/express/requestMock';
 import { responseMock } from '@src/__mocks__/express/responseMock';
 
-beforeAll(() => {
-  jest.useFakeTimers();
-  jest.setSystemTime(new Date('13-06-2022'));
-});
-
 const exceptionMiddleware = new ExceptionMiddleware();
+const next = jest.fn();
 
 describe('Exception Middleware', () => {
   it('should return the APIError as a response if error is typeof APIError', async () => {
     const apiErr = new APIError(400, 'Invalid data');
-    const next = jest.fn();
 
     await exceptionMiddleware.execute(apiErr, requestMock, responseMock, next);
 
@@ -27,7 +22,6 @@ describe('Exception Middleware', () => {
 
   it('should return a defined response if the error raised isn\'t an APIError', async () => {
     const apiErr = new Error('NullPointerException');
-    const next = jest.fn();
 
     const definedError = new APIError(500, 'Server Internal Error, try again.');
 

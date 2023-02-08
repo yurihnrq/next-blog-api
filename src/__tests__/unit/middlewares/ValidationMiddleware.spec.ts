8ca2c26666626ae21a4b7f156433802162bd9245
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2023-02-08 20:44:19 -0300

    feat: ValidationMiddleware should use schema to validate request body

--- src/__tests__/unit/middlewares/ValidationMiddleware.spec.ts
+++ src/__tests__/unit/middlewares/ValidationMiddleware.spec.ts
@@ -1,14 +1,26 @@
 import { ValidationMiddleware } from '@src/middlewares/ValidationMiddleware';
 import { requestMock } from '@src/__mocks__/express/requestMock';
 import { responseMock } from '@src/__mocks__/express/responseMock';
+import { ZodSchema } from 'zod';
 
 const next = jest.fn();
-const validationMiddleware: APIMiddleware = new ValidationMiddleware();
+const schema: ZodSchema = {
+  parse: jest.fn()
+} as unknown as ZodSchema;
+const validationMiddleware: APIMiddleware = new ValidationMiddleware(schema);
 
 describe('ValidationMiddleware', () => {
   it('should call next function', async () => {
     await validationMiddleware.execute(requestMock, responseMock, next);
 
     expect(next).toHaveBeenCalled();
+  });
+
+  it('should use schema to validate request body', async () => {
+    requestMock.body = { name: 'John Doe' };
+
+    await validationMiddleware.execute(requestMock, responseMock, next);
+
+    expect(schema.parse).toHaveBeenCalledWith(requestMock.body);
   });
 });
--- src/middlewares/ValidationMiddleware.ts
+++ src/middlewares/ValidationMiddleware.ts
@@ -1,11 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
+import { ZodSchema } from 'zod';
 
 export class ValidationMiddleware implements APIMiddleware {
+  #schema: ZodSchema;
+
+  constructor(schema: ZodSchema) {
+    this.#schema = schema;
+  }
+
   execute = async (
-    _req: Request,
+    req: Request,
     _res: Response,
     next: NextFunction
   ): Promise<void> => {
+    await this.#schema.parse(req.body);
+
     return next();
   };
 }

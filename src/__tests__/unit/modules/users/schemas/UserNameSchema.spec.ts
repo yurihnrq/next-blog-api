c02c467dde65857a4641774165da7e89cd0f9798
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2023-02-20 13:10:15 -0300

    test: UserNameSchema should throw if name is not a string

--- src/__tests__/unit/modules/users/schemas/UserNameSchema.spec.ts
+++ src/__tests__/unit/modules/users/schemas/UserNameSchema.spec.ts
@@ -6,4 +6,10 @@ describe('UserNameSchema', () => {
       UserNameSchema.parse('John Doe');
     }).not.toThrow();
   });
+
+  it('should throw an error if name is not a string', () => {
+    expect(() => {
+      UserNameSchema.parse(12345678);
+    }).toThrow('Name must be a string.');
+  });
 });
